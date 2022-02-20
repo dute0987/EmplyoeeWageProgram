8a26c2aadceb3cd204ec3b603186c3afad2d06aa
dute0987 <dute0987@gmail.com>
2022-02-20 19:42:46 +0530

    Greeting Message for Emplyoee Wage

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UC_day3
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            WelcomeToEmplyoeeProgram.WelcomeProgram();
+            //UC_1_AbsentOrPresent.EmployeePresentOrAbsent();
+            //UC_2_CalculationOfDailyWage.CalEmpWage();
+            //UC_3_PartTimeFullTime.PartTimeFullTime();
+            //UC_4_EmpCheckUsingCaseStatment.EmpCheckUsingCase();
+        }
+    }
+}
