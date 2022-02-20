8ed27d34b5a057a79a0246b2f8b5b1282fb1d9af
dute0987 <dute0987@gmail.com>
2022-02-20 20:09:13 +0530

    Check Emplyoee Present or Absent

---
+++ UC-1-AbsentOrPresent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC_day3
+{
+    public class UC_1_AbsentOrPresent
+    {
+        /// <summary>
+        ///UC-1-Employees the present or absent.
+        /// </summary>
+        public static void EmployeePresentOrAbsent()
+        {
+            Random random = new Random();
+            int num = random.Next(2);
+
+            if (num == 1)
+            {
+                Console.WriteLine("Emplyoee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emplyoee is Absent");
+            }
+        }
+    }
+}
